b49e16f9f2ed28adddc8ae15d542a3e7c0e22960
Katz <47083415+1uvu@users.noreply.github.com>
2021-12-24 20:09:56 +0800

    update cluster

--- intrenal/info/cluster.go
+++ intrenal/info/cluster.go
@@ -15,22 +15,23 @@ type (
 
 func Run(conf *config.Config) {
 	for _, node := range conf.Nodes {
+		var _logger logger.Logger
 		logger.InitCmdLogger()
 		logger.InitFileLogger(node.Output)
+		if node.Output.LogToFile {
+			_logger = logger.FileLogger
+		} else {
+			_logger = logger.CMDLogger
+		}
 		// the conf has been verified, so don't need to handle error
 		d, _ := time.ParseDuration(node.Duration)
-		if node.Output.DirPath == "" {
-			if d == 0 {
-				run(node, logger.CMDLogger)
-			} else {
-				runWithDuration(node, d, logger.CMDLogger)
-			}
+
+		if d == 0 {
+			go run(node, _logger)
+		} else if d < 0 {
+			go runWithInfinity(node, _logger)
 		} else {
-			if d == 0 {
-				run(node, logger.FileLogger)
-			} else {
-				runWithDuration(node, d, logger.FileLogger)
-			}
+			go runWithDuration(node, _logger)
 		}
 	}
 }
@@ -41,12 +42,44 @@ func run(conf *config.NodeConfig, defaultLogger logger.Logger) {
 	}
 }
 
-func runWithDuration(conf *config.NodeConfig, duration time.Duration, defaultLogger logger.Logger) {
+func runWithInfinity(conf *config.NodeConfig, defaultLogger logger.Logger) {
+	go func() {
+		p, _ := time.ParseDuration(conf.Period)
+		for {
+			time.AfterFunc(p, func() {
+				run(conf, defaultLogger)
+			})
+		}
+	}()
+}
 
+func runWithDuration(conf *config.NodeConfig, defaultLogger logger.Logger) {
+	p, _ := time.ParseDuration(conf.Period)
+	ticker := time.NewTicker(p)
+	ch := make(chan bool)
+	go func(ticker *time.Ticker) {
+		defer ticker.Stop()
+		for {
+			select {
+			case <-ticker.C:
+				run(conf, defaultLogger)
+			case stop := <-ch:
+				if stop {
+					return
+				}
+			default:
+
+			}
+		}
+	}(ticker)
+
+	d, _ := time.ParseDuration(conf.Duration)
+	time.Sleep(d)
+	ch <- true
+	close(ch)
 }
 
 func fetchInfo(conf *config.NodeConfig, option config.InfoOption, defaultLogger logger.Logger) {
-	// ...
 	phyInfo, err := fetchPhyInfo(option, conf.SSH)
 	if err != nil {
 		defaultLogger.Errorf("fetch phy info failed: %s", err.Error())
@@ -54,7 +87,7 @@ func fetchInfo(conf *config.NodeConfig, option config.InfoOption, defaultLogger 
 		//defaultLogger.Info(phyInfo.String())
 		defaultLogger.Info(phyInfo)
 	}
-	// ...
+	// ... fetch more info here
 }
 
 func fetchPhyInfo(option config.InfoOption, sshConfig config.SSHConfig) (*physical.PhyInfo, error) {
