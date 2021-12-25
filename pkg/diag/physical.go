6fc06b1f3e5036928cdb96a28e908fe722320478
Katz <47083415+1uvu@users.noreply.github.com>
2021-12-25 17:26:49 +0800

    del ignoree diag

---
+++ pkg/diag/physical.go
@@ -0,0 +1,47 @@
+package diag
+
+import "github.com/xjlgod/nebula-database-diagnostic/pkg/info/physical"
+
+func GetPhyDiag(info *physical.PhyInfo) (diags []string) {
+	diags = append(diags, "physical nothing.\n")
+
+	if len(diags) > 1 {
+		return diags[1:]
+	}
+	return diags
+}
+
+func processDiag(info *physical.PhyInfo, diags []string) []string {
+	diags = append(diags, "==> diag process info:\n")
+	if info.Process.WaitNumber >= ThresholdWaitNumber {
+		diags = append(diags, "==> process wait number: process wait number is above the threshold.\n")
+		if float64(info.Process.RunNumber/info.CPU.LogicNumber) > ThresholdRunNumber {
+			diags = append(diags, "==> process run number: cpu is too busy.\n")
+		} else {
+			diags = cpuDiag(info, diags)
+			diags = memoryDiag(info, diags)
+		}
+	}
+	return diags
+}
+
+func cpuDiag(info *physical.PhyInfo, diags []string) []string {
+	diags = append(diags, "==> diag cpu info:\n")
+	if float64(info.CPU.IdleTime/100) > ThresholdIdleTime {
+		diags = append(diags, "==> diag cpu idle time: idle time percent is too big\n")
+	}
+
+	if float64(info.CPU.WaitPercent/100) > ThresholdWaitPercent {
+		diags = append(diags, "==> diag cpu wait percent: wait percent is too big. there have io problems\n")
+	}
+
+	return diags
+}
+
+func memoryDiag(info *physical.PhyInfo, diags []string) []string {
+	diags = append(diags, "==> diag memory info:\n")
+	if float64(info.Memory.MemFree/info.Memory.MemTotal) >= ThresholdMemoryFree {
+		diags = append(diags, "==> diag memory free: memory free is too small\n")
+	}
+	return diags
+}
---
+++ pkg/diag/threshold.go
@@ -0,0 +1,20 @@
+package diag
+
+const (
+	/*
+		thresholds about process
+	*/
+	ThresholdWaitNumber = 10
+	ThresholdRunNumber  = 0.6
+
+	/*
+		thresholds about memory
+	*/
+	ThresholdMemoryFree = 0.2
+
+	/*
+		thresholds about cpu
+	*/
+	ThresholdIdleTime    = 0.9
+	ThresholdWaitPercent = 0.2
+)
